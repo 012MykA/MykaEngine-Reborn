04f3def37c31b370b32ffaa2d78a0c783719489b
012MykA <1deadeye1@mail.ru>
2025-12-21 18:55:07 +0700

    OpenGL triangle test

--- MykaEngine/src/Platform/OpenGL/OpenGLContext.cpp
+++ MykaEngine/src/Platform/OpenGL/OpenGLContext.cpp
@@ -1,6 +1,6 @@
 #include "OpenGLContext.hpp"
 
-#include "MykaEngine/Core.hpp"
+#include "MykaEngine/Log.hpp"
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -17,6 +17,11 @@ namespace Myka
         glfwMakeContextCurrent(m_WindowHandle);
         int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
         MYKA_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+        MYKA_CORE_INFO("OpenGL Info:");
+        MYKA_CORE_INFO("  Vendor: {0}", (const char*)glGetString(GL_VENDOR));
+        MYKA_CORE_INFO("  Renderer: {0}", (const char*)glGetString(GL_RENDERER));
+        MYKA_CORE_INFO("  Version: {0}", (const char*)glGetString(GL_VERSION));
     }
 
     void OpenGLContext::SwapBuffers()
