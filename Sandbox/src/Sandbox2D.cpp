90b402a6855e152ac07a65ea661985b4590836a3
012MykA <1deadeye1@mail.ru>
2025-12-30 19:24:48 +0700

    batch rendering rotated objects

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -40,7 +40,9 @@ void Sandbox2D::OnUpdate(Myka::Timestep ts)
         Myka::Renderer2D::DrawQuad({-1.0f, 0.0f}, {0.8f, 0.8f}, {0.8f, 0.2f, 0.3f, 1.0f});
         Myka::Renderer2D::DrawQuad({0.5f, -0.5f}, {0.5f, 0.75f}, {0.2f, 0.3f, 0.8f, 1.0f});
         Myka::Renderer2D::DrawQuad({0.0f, 0.0f, -0.1f}, {1.5f, 1.5f}, m_BoxTexture, 5.0f);
-        Myka::Renderer2D::DrawQuad({1.5f, 1.5f, 0.1f}, {1.0f, 1.0f}, m_BoxTexture);
+
+        static float rotation = 0.0f;
+        Myka::Renderer2D::DrawRotatedQuad({-1.0f, -1.2f, -0.1f}, {1.0f, 1.0f}, glm::radians(rotation++), m_BoxTexture);
 
         Myka::Renderer2D::EndScene();
     }
